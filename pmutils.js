14bbf6b70aba32a342d386e88fdefb6fcf8c2286
Jinggit <jingghster@gmail.com>
2019-06-28 17:42:31 -0400

    Update pmutils.js

--- pmutils.js
+++ pmutils.js
@@ -99,7 +99,7 @@ if (typeof PMUtil === "undefined") {
                 var dateNow = new Date(); 
                 return dateNow.getTime()+(1000 * 60 * 60  * 24 * 30);
             },
-            $randomInt1: function (min, max) {
+            $randomInt: function (min, max) {
                 if (!min) {
                     min = 0;
                     max = 100;
